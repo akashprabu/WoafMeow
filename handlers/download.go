891586fdf30a4cff5bd330d15d96bc045c2298d5
akashprabu <appleakash202@gmail.com>
2024-09-26 19:53:07 -0700

    Fixed a bug

--- handlers/download.go
+++ handlers/download.go
@@ -1,22 +1,26 @@
 package handlers
 
 import (
-    "net/http"
-    "os"
-    "path/filepath"
+	"net/http"
+	"os"
+	"path/filepath"
 )
 
 // DownloadImageHandler serves the processed image for download
 func DownloadImageHandler(w http.ResponseWriter, r *http.Request) {
-    fileName := filepath.Base(r.URL.Path[len("/download/"):])
-    processedPath := filepath.Join("./processed", fileName)
+	fileName := filepath.Base(r.URL.Path[len("/download/"):])
+	processedPath := filepath.Join("./processed", fileName)
 
-    // Check if the file exists
-    if _, err := os.Stat(processedPath); os.IsNotExist(err) {
-        http.Error(w, "File not found", http.StatusNotFound)
-        return
-    }
+	// Check if the file exists
+	if _, err := os.Stat(processedPath); os.IsNotExist(err) {
+		http.Error(w, "File not found", http.StatusNotFound)
+		return
+	}
 
-    // Serve the file
-    http.ServeFile(w, r, processedPath)
+	// Set the header to indicate file download
+	w.Header().Set("Content-Disposition", "attachment; filename="+fileName)
+	w.Header().Set("Content-Type", "image/jpeg") // Set to the appropriate content type
+
+	// Serve the file
+	http.ServeFile(w, r, processedPath)
 }
